075cb05a06131da9d5f16acb83185ef5f1144895
lioanag.p@northeastern.edu <lioanag.p@northeastern.edu>
2026-01-28 10:19:53 -0500

    args for main and ranked

--- __main__.py
+++ __main__.py
@@ -1,18 +1,37 @@
-from helpers import get_puuid, get_match_ids, win_check, win_percentage
+from helpers import get_puuid, get_match_ids, win_check, win_percentage, league_rank, tft_rank
+import sys
 
-game_name = "umihi"
-tag_line = "G2B"
 
-summoner = get_puuid(game_name, tag_line)
-print(summoner)
+def main(args):
+    if len(args) != 1:
+        print("Usage: python3 __main__.py <name#tag>")
+        return
 
-summoner_matches = get_match_ids(summoner['puuid'], 20)
-print(summoner_matches)
+    if "#" not in sys.argv[1]:
+        print("Usage: python3 __main__.py <name#tag> no hash")
+        return
 
-win = win_check(summoner['puuid'], summoner_matches[0])
-print(win)
+    riot_user = sys.argv[1].split("#")
 
-win_ratio = win_percentage(game_name, tag_line)
-print(win_ratio)
+    game_name = riot_user[0]
+    tag_line = riot_user[1]
 
-#https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/umihi/G2B?api_key=RGAPI-3259592f-2b2e-4aed-9a3b-4dac32a7ed60
+    summoner = get_puuid(game_name, tag_line)
+
+    '''
+    # summoner_matches = get_match_ids(summoner['puuid'], 20)
+    # win = win_check(summoner['puuid'], summoner_matches[0])
+    # win_ratio = win_percentage(game_name, tag_line)
+    '''
+
+    summoner_rank_lol = league_rank(summoner['puuid'])
+    print("League rank: " + summoner_rank_lol)
+
+    summoner_rank_tft = tft_rank(summoner['puuid'])
+    print("TFT rank: " + summoner_rank_tft)
+
+    # https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/umihi/G2B?api_key=RGAPI-3259592f-2b2e-4aed-9a3b-4dac32a7ed60
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
--- helpers.py
+++ helpers.py
@@ -2,7 +2,8 @@ import requests
 from urllib.parse import urlencode
 import settings
 
-def get_puuid(game_name=None, tag_line=None, region = settings.DEFAULT_REGION):
+
+def get_puuid(game_name=None, tag_line=None, region=settings.DEFAULT_REGION):
     '''
     Wrapper for ACCOUNT-V1 API portal
     Gets player info from SUMMONER based on name
@@ -33,7 +34,8 @@ def get_puuid(game_name=None, tag_line=None, region = settings.DEFAULT_REGION):
         print(f"Error getting player info: {e}")
         return None
 
-def get_match_ids(summoner_puuid, matches_count, region = settings.DEFAULT_REGION):
+
+def get_match_ids(summoner_puuid, matches_count, region=settings.DEFAULT_REGION):
     '''
     get match IDs from summoner
 
@@ -43,13 +45,11 @@ def get_match_ids(summoner_puuid, matches_count, region = settings.DEFAULT_REGIO
     :return: list of match IDs
     '''
 
-
-
     params = {
         'api_key': settings.API_KEY
     }
     api_url = (f"https://{region}.api.riotgames.com/lol/match/v5/matches/by-puuid/{summoner_puuid}/ids")
-                #f"?start=0&count={matches_count}")
+    # f"?start=0&count={matches_count}")
 
     try:
         response = requests.get(api_url, params=urlencode(params))
@@ -59,7 +59,8 @@ def get_match_ids(summoner_puuid, matches_count, region = settings.DEFAULT_REGIO
         print(f"Error getting summoner match data from API: {e}")
         return None
 
-def win_check(summoner_puuid, match__id, region = settings.DEFAULT_REGION):
+
+def win_check(summoner_puuid, match__id, region=settings.DEFAULT_REGION):
     params = {
         'api_key': settings.API_KEY,
     }
@@ -81,15 +82,89 @@ def win_check(summoner_puuid, match__id, region = settings.DEFAULT_REGION):
     player_info = match_data['info']['participants'][player_index]
     return player_info['win']
 
-def win_percentage(summoner, tag_line, region = settings.DEFAULT_REGION):
+
+def win_percentage(summoner, tag_line, region=settings.DEFAULT_REGION):
     summoner = get_puuid(summoner, tag_line, region)
     matches = get_match_ids(summoner['puuid'], 20, region)
 
     wins = 0
     for match in matches:
         if win_check(summoner['puuid'], match):
-            print('win')
             wins += 1
-        else:
-            print('loss')
-    return (wins/len(matches))*100
+    return (wins / len(matches)) * 100
+
+
+# tft-league-v1
+# needs puuid
+
+def league_rank(summoner_puuid, region=settings.DEFAULT_REGION_CODE):
+    """
+        Gets league rank of summoner
+
+        :param summoner_puuid: Summoner PUUID
+        :param region: region where summon is located
+        :return: str league rank
+    """
+    params = {
+        'api_key': settings.API_KEY
+    }
+    api_url = f'https://{region}.api.riotgames.com/lol/league/v4/entries/by-puuid/{summoner_puuid}'
+    # https://na1.api.riotgames.com/lol/league/v4/entries/by-puuid/nLLQW7J5kJSJS7R5fcOPVhgIl4_xTeSK_uplIU5Z5-1GT1rEMCQW1JFHirWnX3bl2tQURV6Ji0gfLA
+
+    rank = ''
+
+    try:
+        response = requests.get(api_url, params=urlencode(params))
+        response.raise_for_status()
+        player_data = response.json()
+    except requests.exceptions.RequestException as e:
+        print(f"Error getting match data from API: {e}")
+        return None
+
+    for attribute in player_data:
+        if attribute['queueType'] == 'RANKED_SOLO_5x5':
+            tier = attribute['tier']
+            level = attribute['rank']
+            rank = tier + " " + level
+            break
+
+    if rank == '':
+        rank = 'Unranked'
+
+    return rank
+
+def tft_rank(summoner_puuid, region=settings.DEFAULT_REGION_CODE):
+    """
+        Gets TFT rank of summoner
+
+        :param summoner_puuid: Summoner PUUID
+        :param region: region where summon is located
+        :return: str tft rank
+    """
+    params = {
+        'api_key': settings.API_KEY
+    }
+    api_url = f'https://{region}.api.riotgames.com/tft/league/v1/by-puuid/{summoner_puuid}'
+    # https://na1.api.riotgames.com/tft/league/v1/by-puuid/nLLQW7J5kJSJS7R5fcOPVhgIl4_xTeSK_uplIU5Z5-1GT1rEMCQW1JFHirWnX3bl2tQURV6Ji0gfLA
+
+    rank = ''
+
+    try:
+        response = requests.get(api_url, params=urlencode(params))
+        response.raise_for_status()
+        player_data = response.json()
+    except requests.exceptions.RequestException as e:
+        print(f"Error getting match data from API: {e}")
+        return None
+
+    for attribute in player_data:
+        if attribute['queueType'] == 'RANKED_TFT':
+            tier = attribute['tier']
+            level = attribute['rank']
+            rank = tier + " " + level
+            break
+
+    if rank == '':
+        rank = 'Unranked'
+
+    return rank
